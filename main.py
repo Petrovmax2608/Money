86bfd1c81635092ae4c35588a22cf778ec290165
Max <31863112+Petrovmax2608@users.noreply.github.com>
2024-12-05 18:48:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from telegram.ext import (
     CallbackContext,
 )
 
-# Настройка логирования
+
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
